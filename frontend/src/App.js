fa127ce1a5a830c6867f8dd3c5a87060c999d512
Ricardo Maia <rsmaia@gmail.com>
2021-01-11 02:49:28 -0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid_v4 } from "uuid";
 
 function App() {
+  const url = "http://localhost:5000/api";
+  const [users, setUsers] = useState([]);
 
-  const url =  "http://localhost:5000/api";
-  fetch(url)
-  .then(response => response.json())
-    .then((jsonData) => {
-      // jsonData is parsed json object received from url    
-      console.log(jsonData)
-    })
-    .catch((error) => {
-      // handle your errors here
-      console.error(error)
-    })
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setUsers(data.users));
 
-    return <div>
-      <h2>Example</h2>
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div className="card" key={uuid_v4()}>
+          <h3>
+            {user.name.last}, {user.name.first} - Age: {user.age}
+          </h3>
+          <p>phone: {user.phone}</p>
+          <p>email: {user.email}</p>
+          <p>Address: {user.address}</p>
+        </div>
+      ))}
     </div>
-
+  );
 }
 
 export default App;
